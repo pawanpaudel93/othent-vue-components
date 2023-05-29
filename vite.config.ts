cf627c92d32f10b800ae8e1032c9820fab54c6aa
Pawan Paudel <pawanpaudel93@gmail.com>
2023-05-29 23:46:38 +0545

    fix: include css into the build

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,19 @@ import vue from "@vitejs/plugin-vue";
 import { resolve } from "node:path";
 import dts from "vite-plugin-dts";
 
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), dts()],
+  plugins: [vue(), dts(), cssInjectedByJsPlugin()],
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
     },
   },
   build: {
+    cssCodeSplit: true,
+    target: "esnext",
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
       name: "OthentVueComponents",
